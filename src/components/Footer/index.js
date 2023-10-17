3edb47526a0de9d446ad548b49e4f7f3e0380da1
teixeirajrwd <teixeirajrwd@gmail.com>
2023-10-17 12:06:22 -0300

    Linkedin adcionado ao meu nome no footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,7 +2,7 @@ import styles from "./Footer.module.css"
 function Footer (){
     return(
         <footer className={styles.footer}>
-            Todos os direitos Reservados &copy; MyPackageBR - Developed by Jun1or Te1xe1ra - 2023.
+            Todos os direitos Reservados &copy; MyPackageBR - Developed by <a href="https://www.linkedin.com/in/junior-teixeiradev/">Jun1or Te1xe1ra</a> - 2023.
             <div className={styles.credits}>
                 Public Api by <a href='https://linketrack.com/' target="_blank">Link & Track</a>
             </div>
